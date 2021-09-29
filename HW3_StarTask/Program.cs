454a54eedc66b99af4acd6ecc26ed3dcbaa378e5
TemporarYLife <aay23@tpu.ru>
2021-09-29 17:00:56 +0700

    Done homework

--- HW3_StarTask/Program.cs
+++ HW3_StarTask/Program.cs
@@ -10,6 +10,56 @@ namespace HW3_StarTask
     {
         static void Main(string[] args)
         {
+            string sentence=Console.ReadLine();
+            string[] splitted;
+            string left_side,right_side, max_palindrom="";
+            int middle_index, palindrom_length=0;
+            splitted=sentence.Split(' ');
+            for(int i=0; i<splitted.Length; i++)
+            {
+                left_side="";
+                right_side="";
+                if (splitted[i].Length%2==0)
+                {
+                    middle_index=splitted[i].Length/2;
+                   for (int j=0; j<middle_index;j++)
+                    {
+                        left_side+=splitted[i][j];
+  
+                    }
+                    for (int k=splitted[i].Length-1;k>=middle_index;k--)
+                    {
+                        right_side+=splitted[i][k];
+                        
+                    }
+                }
+                else
+                {
+                    middle_index=(splitted[i].Length-1)/2;
+                    for (int j=0; j<=middle_index;j++)
+                    {
+                        left_side+=splitted[i][j];
+
+                    }
+                    for (int k=splitted[i].Length-1;k>=middle_index;k--)
+                    {
+                        right_side+=splitted[i][k];
+                        
+                    }
+                }
+                 if (left_side.ToLower()==right_side.ToLower())
+                {
+                    if (splitted[i].Length>palindrom_length)
+                    {
+                        palindrom_length=splitted[i].Length;
+                        max_palindrom=splitted[i];
+
+                    }
+                    
+                }
+            }
+            Console.WriteLine($"Самый длинный палиндром - {max_palindrom}");
+            Console.ReadKey();
         }
     }
 }
