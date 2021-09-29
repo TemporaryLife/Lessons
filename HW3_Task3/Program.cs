93552f09cdd9025721519c6208077a4fb4016dd5
TemporarYLife <aay23@tpu.ru>
2021-09-29 14:55:00 +0700

    Last task remain

---
+++ HW3_Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase=Console.ReadLine();
+            for(int i=phrase.Length-1;i>=0; i--)
+            {
+                Console.Write(phrase[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
