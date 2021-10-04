2f3efef5a6573a341f6189c351bec6920064b627
TemporarYLife <aay23@tpu.ru>
2021-10-04 13:42:55 +0700

    almost did

---
+++ HW4_StarTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ttt
+{
+    class Program
+    {
+        public static int[] array_1 = new[] {5, 6, 10};
+        public static int[] array_2 = new[] {1, 4};
+        static void Main(string[] args)
+        {
+            double a=Median();
+            Console.WriteLine($"Медиана двух заданных списков равна {a}");
+        }
+
+        static int[] JoinArray(int[] arr1, int[] arr2)
+        {
+            var result = new int[arr1.Length + arr2.Length];
+            arr1.CopyTo(result,0);
+            arr2.CopyTo(result, arr1.Length);
+            Array.Sort(result);
+            return result;
+        }
+
+        static double Median()
+        {
+            var res=JoinArray(array_1, array_2);
+            if (res.Length % 2 == 0)
+            {
+                return ((double)res[res.Length / 2] + res[res.Length / 2 -1]) / 2;
+            }
+            else
+            {
+                return res[res.Length / 2];
+            }
+        }
+    }
+}
---
+++ HW4_Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();
+            int res=sum(input);
+            Console.WriteLine(res);
+        }
+
+        static int sum(string input)
+        {
+            int number,summa=0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int.TryParse(Convert.ToString(input[i]), out number);
+                summa += number;
+            }
+
+            return summa;
+        }
+        
+    }
+}
