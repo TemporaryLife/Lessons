389474cccdc7bcdc46ae3e0d321059d6a74f85b2
TemporarYLife <aay23@tpu.ru>
2021-10-06 14:34:59 +0700

    Done 1/2/3 Tasks

---
+++ HW5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileText = Console.ReadLine();
+            string path =@"C:\Users\Konstantin\source\repos\TemporaryLife\Lessons\HW5\Task1.txt" ;
+            FileCW(FileText, path ); 
+        }
+
+
+        static void FileCW(string text, string path)
+        {
+            File.WriteAllText(path, text);
+        }
+    }
+}
---
+++ HW5_Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Konstantin\source\repos\TemporaryLife\Lessons\HW5_Task2\startup.txt";
+            string text;
+            Console.Write("Хотите ли вы внести изменения в файл?(Да/Нет) ");
+            string ans=Console.ReadLine();
+
+            if (ans == "Да")
+            {
+                Console.WriteLine(@"Перезаписать в файл? Если дан ответ 'Нет', то изменения просто добавятся в конец файла ");
+                ans = Console.ReadLine();
+                if (ans == "Да")
+                {
+                    text = Console.ReadLine();
+                    File.WriteAllText(path, $"{text}   ({DateTime.Now})\n");
+                }
+                else if (ans == "Нет")
+                {
+                    text = Console.ReadLine();
+                    File.AppendAllText(path, $"{text}   ({DateTime.Now})\n");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода");
+                    return;
+                }
+
+            }
+            else if (ans == "Нет")
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода");
+                Console.WriteLine($"Дата последнего доступа к файлу: {DateTime.Now}");
+                return;
+            }
+
+
+        }
+    }
+}
