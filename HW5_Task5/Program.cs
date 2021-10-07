648140d21db7ef3e5b42db310633dc652298bddf
TemporarYLife <aay23@tpu.ru>
2021-10-07 08:44:08 +0700

    Class Created at Task5

---
+++ HW5_Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDo Task1=new ToDo("Почистить печь", false);
+            Task1.IsDone = true;
+            Task1.Title = "123";
+            string json = JsonSerializer.Serialize(Task1);
+            File.WriteAllText("new.json",json );
+        }
+        
+    }
+}
---
+++ HW5_Task5/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HW5_Task5
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; } 
+        public ToDo(string Title, bool IsDone)
+        {
+            this.Title = Title;
+            this.IsDone = IsDone;
+        }
+    }
+}
